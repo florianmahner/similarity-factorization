bc100aa773b825656f2759b16c14a69b72a2fca6
florianmahner <florian.mahner@icloud.com>
2025-10-01 13:25:24 +0200

    Update test

--- tests/cv_test.py
+++ tests/cv_test.py
@@ -7,12 +7,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from cross_validation import cross_val_score
+from _compute_prange import estimate_p_bound_fast
+from matplotlib.colors import LogNorm
 
 
 def simulate_data(n_samples: int = 10, rank: int = 5, seed: int = None):
 
     rng = np.random.default_rng(seed)
-
     w_true = rng.random(size=(n_samples, rank))
     x_true = w_true @ w_true.T
     return x_true
@@ -22,25 +23,25 @@ def main(
     n_samples: int = 200,
     rank: int = 10,
     seed: int = 0,
-    observed: float = 0.3,
-    rho: float = 1.0,
 ):
 
     x_true = simulate_data(n_samples, rank)
-    rank_range = list(range(max(1, rank - 5), rank + 6))
+    rank_range = list(range(max(1, rank - 10), rank + 10))
+
+    p_min, p_max, _ = estimate_p_bound_fast(x_true, random_state=seed)
+    print("Average p bounds: ", 0.5 * (p_min + p_max))
 
     scorer = cross_val_score(
         x_true,
         param_grid={
             "rank": rank_range,
-            "max_outer": [200],
-            "max_inner": [50],
-            "rho": [rho],
             "tol": [0.0],
+            "max_outer": [50],
+            "max_inner": [30],
             "init": ["random_sqrt"],
         },
-        n_repeats=5,
-        observed_fraction=observed,
+        n_repeats=10,
+        observed_fraction=0.5 * (p_min + p_max),
         n_jobs=-1,
         random_state=seed,
         verbose=0,
@@ -73,25 +74,10 @@ if __name__ == "__main__":
     parser.add_argument("--n-samples", type=int, default=200, help="Number of samples")
     parser.add_argument("--rank", type=int, default=10, help="True rank of the matrix")
     parser.add_argument("--seed", type=int, default=0, help="Random seed")
-    parser.add_argument(
-        "--observed",
-        type=float,
-        default=0.3,
-        help="Fraction of observed entries (0, 1]",
-    )
-
-    parser.add_argument(
-        "--rho",
-        type=float,
-        default=10.0,
-        help="Regularization parameter",
-    )
 
     args = parser.parse_args()
     main(
         n_samples=args.n_samples,
         rank=args.rank,
         seed=args.seed,
-        observed=args.observed,
-        rho=args.rho,
     )
