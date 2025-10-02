f11729fd8d8fc47ceec110427ec897d8b8e1c647
florianmahner <florian.mahner@icloud.com>
2025-10-02 10:58:07 +0200

    Remove tools from src/ to prepare for root-level subtree

--- src/tools/extraction/_base.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class BaseModelLoader(ABC):
-    @staticmethod
-    @abstractmethod
-    def load(model_name: str, weights: str | None = "DEFAULT"):
-        pass
