8e2d0a576b0c2275c722339adf6033c9dfa75790
florianmahner <florian.mahner@icloud.com>
2025-10-06 15:18:06 +0200

    Remove root tools subtree

--- tools/extraction/__init__.py
+++
@@ -1,8 +0,0 @@
-from .extractor import extract_features_from_model, load_model, build_feature_extractor
-from ._hooks import HookModel
-
-__all__ = [
-    "extract_features_from_model",
-    "load_model",
-    "build_feature_extractor",
-]
